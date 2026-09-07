820919e4196af5f2c4b0095a7a003de2bbb17558
Alex Douglas <alexd106@gmail.com>
2026-09-07 16:13:13 +0100

    update git exercise to included screenshots and reorientate to .Rmd changes

--- docs/exercise_solutions/exercise_9_solution.R
+++ docs/exercise_solutions/exercise_9_solution.R
@@ -1,20 +1,15 @@
-## ----Q1, echo=TRUE------------------------------------------------------------
+## ----Q1, echo=TRUE, class.source="fold-none"----------------------------------
 library(usethis)
 
 git_sitrep()
 
 
-## ----Q4, echo=SOLUTIONS-------------------------------------------------------
-cardiac <- read.table('data/cardiacdata.txt', header = TRUE, sep = "\t",
-                      stringsAsFactors = TRUE)
-
-cardiac$Fsmoking <- factor(cardiac$smoking, levels = c(1, 2, 3),
-                           labels = c("Current", "Ex", "Never"))
-
-boxplot(hdlchol ~ Fsmoking, data = cardiac,
-        xlab = "Smoking status", ylab = "HDL cholesterol (mmol/l)")
-
-
 ## ````{.md .foldable}
-## output/
+## cardiac_report.pdf
+## ````
+## 
+## Or, to catch every PDF the Project ever produces:
+## 
+## ````{.md .foldable}
+## *.pdf
 ## ````
